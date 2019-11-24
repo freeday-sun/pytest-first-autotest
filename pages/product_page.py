00a6797dc702c92bbb5de9dc97e4558d43d444f2
freeday-sun <gigdeer@yandex.ru>
2019-11-24 20:04:26 +0300

    add more parametrize in products page

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,9 +13,10 @@ class ProductPage(BasePage):
             self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE_EQUAL_REAL_PRODUCT_NAME)
         real_product_name = self.browser.find_element(*ProductPageLocators.REAL_PRODUCT_NAME)
         assert product_name_in_message.text == real_product_name.text, \
-            "{} real {}".format(product_name_in_message.text, real_product_name.text)
+            'alert product name: "{}" real product name: "{}"'.format(product_name_in_message.text, real_product_name.text)
 
     def product_price_equal_basket_amount(self):
         basket_amount = self.browser.find_element(*ProductPageLocators.BASKET_AMOUNT_IN_MESSAGE)
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
-        assert basket_amount.text == product_price.text, "{} real {}".format(basket_amount.text, product_price.text)
+        assert basket_amount.text == product_price.text,\
+            'alert basket amount: "{}" real product price: "{}"'.format(basket_amount.text, product_price.text)
--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,12 @@
 import pytest
 from .pages.product_page import ProductPage
 
-links = ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
-         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        ]
 
-
-@pytest.mark.parametrize("product", links)
-def test_guest_can_add_product_to_basket(browser, product):
-    link = product
+@pytest.mark.parametrize('promo_offer', ["0", "1", "3", "4", "5", "6", \
+                                         pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}".format(promo_offer)
+    print(link)
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()
     page.add_product_to_basket()
