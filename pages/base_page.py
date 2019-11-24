f68eb20cbac60c58b0d18559e2f42dc837319744
freeday-sun <gigdeer@yandex.ru>
2019-11-24 19:16:55 +0300

    add parametrize in products page

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,14 +5,27 @@ from math import log, sin
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
+        """Base class for the web page
+
+        :param selenium.webdriver.Remote browser: Selenium WebDriver instance
+        :param str url: Link to the page
+        :param int timeout: implicitly wait timeout
+        """
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
     def open(self):
+        """Open the given web page in the browser
+
+        :return: None
+        """
         self.browser.get(self.url)
 
     def solve_quiz_and_get_code(self):
+        """ Method for solving alert quiz
+
+        """
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(log(abs((12 * sin(float(x))))))
@@ -27,6 +40,12 @@ class BasePage():
             print("No second alert presented")
 
     def is_element_present(self, how, what):
+        """ Check that the element exists
+
+        :param how: Choosing a selector search method
+        :param what: Element selector
+        :return bool:
+        """
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,14 @@
+import pytest
 from .pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+links = ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        ]
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize("product", links)
+def test_guest_can_add_product_to_basket(browser, product):
+    link = product
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()
     page.add_product_to_basket()
